a7018f6c7598cac514a3d5273418afbf4b5b449a
Maksim <51973908+zaiets-maksim@users.noreply.github.com>
2020-10-15 00:35:12 +0300

    commit

--- Points/Assets/MovementPath.cs
+++ Points/Assets/MovementPath.cs
@@ -11,19 +11,27 @@ public PathTypes PathType;
 public int movementDirection = 1;
 public int movingTo = 0;
 public Transform[] PathElements;
-public GameObject Rocket;
+//public GameObject Rocket;
+public LineRenderer lineRenderer;
 
+Vector2 PointPos1;
+Vector2 PointPos2;
 
 int _lenght;
 void Start(){
 _lenght = PathElements.Length;
-}
-public void OnDrawGizmos(){
-    if(PathElements == null || PathElements.Length < 2)
-    return;
+lineRenderer = GetComponent<LineRenderer>();
 
         for (int i = 1; i < _lenght; i++){
-    Gizmos.DrawLine(PathElements[i - 1].position, PathElements[i].position);}
+            PointPos1 = new Vector2(PathElements[i - 1].transform.position.x, PathElements[i - 1].transform.position.y);
+            PointPos2 = new Vector2(PathElements[i].transform.position.x, PathElements[i].transform.position.y);
+            
+            lineRenderer.SetPosition(i-1, new Vector3(PointPos1.x, PointPos1.y, 0));
+            lineRenderer.SetPosition(i, new Vector3(PointPos2.x, PointPos2.y, 0));
+    //Gizmos.DrawLine(PathElements[i - 1].position, PathElements[i].position);}
+        }
+        if(PathElements == null || PathElements.Length < 2)
+        return;
 
     if(PathType == PathTypes.loop)
     Gizmos.DrawLine(PathElements[0].position, PathElements[PathElements.Length - 1].position);
